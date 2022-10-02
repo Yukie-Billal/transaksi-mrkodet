f0172bd5e5846fc2d0fd74017ee2a250f1243faf
GMNocK <userwith.immortal@gmail.com>
2022-10-02 19:32:32 +0700

    Simple Database Design In Laravel system(seed,factory,migrate,model), add table transaksi

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $guarded = [
+        'id'
+    ];
+
+    public function transaksi()
+    {
+        return $this->hasMany(transaksi::class, 'user_id');
+    }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -643,6 +643,7 @@ class ComposerStaticInit4ce44e88ff0ed1d09cadf120cff20398
         'Cron\\HoursField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/HoursField.php',
         'Cron\\MinutesField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/MinutesField.php',
         'Cron\\MonthField' => __DIR__ . '/..' . '/dragonmantank/cron-expression/src/Cron/MonthField.php',
+        'Database\\Factories\\TransaksiFactory' => __DIR__ . '/../..' . '/database/factories/TransaksiFactory.php',
         'Database\\Factories\\UserFactory' => __DIR__ . '/../..' . '/database/factories/UserFactory.php',
         'Database\\Seeders\\DatabaseSeeder' => __DIR__ . '/../..' . '/database/seeders/DatabaseSeeder.php',
         'DeepCopy\\DeepCopy' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/DeepCopy.php',
